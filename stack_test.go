c34db9974365338a4c750894480dfbb6fdac73a0
Simon Dann <carbontwelve@users.noreply.github.com>
2017-05-26 10:10:12 +0100

    :white_check_mark: added stack test to check against c++11 std::stack

---
+++ stack_test.go
@@ -0,0 +1,37 @@
+package main
+
+import "testing"
+
+// Based upon example here http://en.cppreference.com/w/cpp/container/stack/top
+func TestStack(t *testing.T) {
+	s := NewStack()
+	s.Push(2)
+	s.Push(6)
+	s.Push(51)
+
+	if s.Len() != 3 {
+		t.Error("Expected Len to return 3, got ", s.Len())
+	}
+
+	if (s.Peek() != 51) {
+		t.Error("Expected Peek to return 51, got ", s.Peek())
+	}
+
+	if s.Len() != 3 {
+		t.Error("Expected Len to return 3, got ", s.Len())
+	}
+
+	p, _ := s.Pop()
+
+	if p != 51 {
+		t.Error("Expected Pop to return 51, got ", p)
+	}
+
+	if s.Len() != 2 {
+		t.Error("Expected Len to return 2, got ", s.Len())
+	}
+
+	if (s.Peek() != 6) {
+		t.Error("Expected Peek to return 6, got ", s.Peek())
+	}
+}
